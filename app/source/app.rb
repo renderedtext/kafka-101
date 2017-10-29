41f997453738da9199a219a4309b2994f9ee8613
Igor Šarčević <igor@renderedtext.com>
2017-10-29 20:25:57 +0100

    Docker compose setup and message producer

---
+++ app/source/app.rb
@@ -0,0 +1,27 @@
+require "kafka"
+
+sleep 10 # give time to kafka to start up
+
+options = {
+  # At least one of these nodes must be available:
+  :seed_brokers => ["kafka:9092"],
+
+  # Set an optional client id in order to identif
+  :client_id => "message-source-1"
+}
+
+$kafka = Kafka.new(options)
+
+message_index = 0
+
+loop do
+  message = "Hello, #{message_index}"
+
+  p "Publishing: #{message}"
+
+	$kafka.deliver_message(message, :topic => "greetings")
+
+  message_index += 1
+
+  sleep 10
+end
